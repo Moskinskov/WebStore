9233cc495d4dc2ae6cd135dabe4e0a20dbe22269
IlyaMoskinskov <moskinskov@gmail.com>
2021-12-12 19:53:31 +0300

    added employees-view

--- WebStore/Controllers/Home.cs
+++ WebStore/Controllers/Home.cs
@@ -1,9 +1,18 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Model;
 
 namespace WebStore.Controllers;
 
 public class Home : Controller
 {
+    private static Employee[] employees = new[]
+    {
+        new Employee() { Id = 0, FirstName = "Илья", LastName = "Иванов", Patronymic = "Иванович", Age = 13 },
+        new Employee() { Id = 1, FirstName = "Максим", LastName = "Иванов", Patronymic = "Ильич", Age = 31 },
+        new Employee() { Id = 2, FirstName = "Софья", LastName = "Ива", Patronymic = "Максимовна", Age = 124 },
+        new Employee() { Id = 3, FirstName = "Илья", LastName = "Ивановов", Patronymic = "Иванович", Age = 32 },
+    };
+
     public IActionResult Index()
     {
         return View();
@@ -14,5 +23,10 @@ public class Home : Controller
         return $"id - {id}\n" +
                $"v1 - {arg0}\n" +
                $"v2 - {arg1}";
+    }
+
+    public ViewResult GetEmployees()
+    {
+        return View(employees);
     }
 }
