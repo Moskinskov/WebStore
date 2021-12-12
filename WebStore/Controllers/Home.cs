bf49454df6bd40a6259429e28bd638ada985e1fa
IlyaMoskinskov <moskinskov@gmail.com>
2021-12-12 19:21:37 +0300

    added views and configured-action

--- WebStore/Controllers/Home.cs
+++ WebStore/Controllers/Home.cs
@@ -4,9 +4,15 @@ namespace WebStore.Controllers;
 
 public class Home : Controller
 {
-    // GET
     public IActionResult Index()
     {
-        return Content("in 'index'-method");
+        return View();
+    }
+
+    public string ConfiguredAction(string id, string arg0, string arg1)
+    {
+        return $"id - {id}\n" +
+               $"v1 - {arg0}\n" +
+               $"v2 - {arg1}";
     }
 }
