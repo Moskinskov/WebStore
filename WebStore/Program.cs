015e4ab3fed3c21aa2e2664bb67d3b9400a94f57
IlyaMoskinskov <moskinskov@gmail.com>
2021-12-12 18:15:21 +0300

    setup route

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,6 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseRouting();
+
+// app.MapDefaultControllerRoute();
+app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+
 
 app.Run();
