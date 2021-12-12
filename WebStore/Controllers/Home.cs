ebdfdb12b71df862746a9faaf070bda6deacf71d
IlyaMoskinskov <moskinskov@gmail.com>
2021-12-12 18:14:46 +0300

    update project to .net-6

---
+++ WebStore/Controllers/Home.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers;
+
+public class Home : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return Content("in 'index'-method");
+    }
+}
