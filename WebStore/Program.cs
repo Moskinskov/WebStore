ba0cf6440df1bfa52aa07583b436be0c82f3383c
IlyaMoskinskov <moskinskov@gmail.com>
2021-12-20 18:43:36 +0300

    added and registered employee-service

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -1,10 +1,12 @@
 using WebStore.Infrastructure.Convensions;
 using WebStore.Infrastructure.Middleware;
+using WebStore.Services.Employee;
 
 var builder = WebApplication.CreateBuilder(args);
 
 IServiceCollection services = builder.Services;
 services.AddControllersWithViews(options => { options.Conventions.Add(new TestConvension()); });
+services.AddSingleton<IEmployeesService, InMemoryEmployeesService>();
 
 
 var app = builder.Build();
---
+++ WebStore/Services/Employee/InMemoryEmployeesService.cs
@@ -0,0 +1,67 @@
+﻿using WebStore.Data;
+
+namespace WebStore.Services.Employee;
+
+class InMemoryEmployeesService : IEmployeesService
+{
+    private readonly List<Model.Employee> employees;
+    private int maxFreeId;
+
+
+    public InMemoryEmployeesService()
+    {
+        employees = TestData.Employees.ToList();
+        maxFreeId = employees.Count;
+    }
+
+    public IEnumerable<Model.Employee> GetAll()
+    {
+        return employees;
+    }
+
+    public Model.Employee? GetById(int id)
+    {
+        return employees.FirstOrDefault(employee => employee.Id == id);
+    }
+
+    public int Add(Model.Employee employee)
+    {
+        if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+        if (!employees.Contains(employee))
+        {
+            employee.Id = maxFreeId++;
+            employees.Add(employee);
+        }
+
+        return employee.Id;
+    }
+
+    public bool Edit(Model.Employee employee)
+    {
+        if (employee is null) throw new ArgumentNullException(nameof(employee));
+
+        if (employees.Contains(employee)) return true;
+
+        Model.Employee? oldEmployee = GetById(employee.Id);
+        if (oldEmployee is null) return false;
+        else
+        {
+            oldEmployee.FirstName = employee.FirstName;
+            oldEmployee.LastName = employee.LastName;
+            oldEmployee.Patronymic = employee.Patronymic;
+            oldEmployee.Age = employee.Age;
+            oldEmployee.TotalDaysInCompany = employee.TotalDaysInCompany;
+            return true;
+        }
+    }
+
+    public bool Delete(int id)
+    {
+        Model.Employee? employee = GetById(id);
+        if (employee is null) return false;
+
+        employees.Remove(employee);
+        return true;
+    }
+}
